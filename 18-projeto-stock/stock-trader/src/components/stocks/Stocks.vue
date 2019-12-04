e89990e79831acc50e08603cd1d8f16d715a0b90
Thiago Anjos <thiagoadosanjos@outlook.com.br>
2019-12-03 23:05:09 -0200

    Iniciando Stock Trader

---
+++ 18-projeto-stock/stock-trader/src/components/stocks/Stocks.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-layout>
+</template>
+
+<script>
+import Stock from './Stock'
+export default {
+  components: {Stock},
+  data(){
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110},
+        { id: 2, name: 'Apple', price: 300},
+        { id: 3, name: 'Spotify', price: 250},
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
